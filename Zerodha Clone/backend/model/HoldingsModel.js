47e2606704a0af15398167983724b43aaa2dca87
ShanmukhaSatvik <satvikshanmukha27@gmail.com>
2025-05-25 15:22:30 +0530

    Delete Zerodha Clone directory

--- Zerodha Clone/backend/model/HoldingsModel.js
+++
@@ -1,9 +0,0 @@
-const {Schema}=require("mongoose");
-const {model}=require("mongoose");
-const HoldingsSchema=new Schema({
-    name: String,
-    qty: Number,
-    price: Number,
-});
-const HoldingsModel=new model("holding",HoldingsSchema);
-module.exports={HoldingsModel};
