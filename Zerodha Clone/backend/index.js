e9f633ed0be17d58b981f2f88cd7da36ba353a9a
ShanmukhaSatvik <satvikshanmukha27@gmail.com>
2025-05-28 20:00:16 +0530

    Changes

--- Zerodha Clone/backend/index.js
+++ Zerodha Clone/backend/index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 8080;
 const uri = process.env.MONGO_URL;
 const app = express();
 app.use(cors({
-    origin: ["https://frontend-idk2.onrender.com", "https://dashboard-w5dz.onrender.com/"],
+    origin: ["https://frontend-idk2.onrender.com", "https://dashboard-1n6h.onrender.com"],
     credentials: true
 }));
 app.use(express.json());
